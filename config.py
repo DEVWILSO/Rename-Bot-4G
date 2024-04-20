5188881608b379e07cb6d2d95032d625a5009691
DevWilson <106603032+DEVWILSO@users.noreply.github.com>
2024-04-20 10:45:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6102180205:AAEKLprCQhVe5yxZkjP4-zMLrYj1C653TEo")
+API_ID = int(os.environ.get("API_ID", "16229284"))
+API_HASH = os.environ.get("API_HASH", "ebd1fead3cc15343bea10b5c164165ba")
+OWNER = int(os.environ.get("OWNER", "1103137195"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "MD_Turbo_rename_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Team_MDL")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002088535757"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://moviesdudebot:MDbots@cluster0.y4gwugw.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "Renamebot")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
